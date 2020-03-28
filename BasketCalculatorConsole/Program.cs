5c0a9a57595ad28387584b3844d7c199ecef24f6
Oliver Garvey <ogarvey@gmail.com>
2020-03-28 13:41:00 +0000

    Initial Commit

---
+++ BasketCalculatorConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasketCalculatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
